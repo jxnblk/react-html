fcf6d1efd717b9cafa6dc543b52094e23db39eee
Brent Jackson <jacksonblack@gmail.com>
2015-02-28 17:22:36 -0500

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# React HTML
+
+Basic HTML React component for page layout
+
+## Getting Started
+
+```bash
+npm install react-html
+```
+
+
+
+MIT License
+
---
+++ html.js
@@ -0,0 +1,36 @@
+
+var React = require('react');
+
+var Head = React.createClass({displayName: "Head",
+  render: function() {
+    return (
+      React.createElement("head", null, 
+        React.createElement("meta", {charSet: "utf-8"}), 
+        React.createElement("title", null, this.props.title), 
+        React.createElement("meta", {name: "viewport", content: "width=device-width,initial-scale=1.0"}), 
+        React.createElement("link", {rel: "stylesheet", href: this.props.stylesheet})
+      )
+    )
+  }
+});
+
+module.exports = React.createClass({displayName: "exports",
+
+  render: function() {
+    var init = {
+      __html: "window.INITIAL_PROPS = " + JSON.stringify(this._owner.props) + ";\n"
+    };
+    return (
+      React.createElement("html", null, 
+        React.createElement(Head, React.__spread({},  this.props)), 
+        React.createElement("body", null, 
+          this.props.children, 
+          React.createElement("script", {dangerouslySetInnerHTML: init}), 
+          React.createElement("script", {src: this.props.script})
+        )
+      )
+    )
+  }
+
+});
+
